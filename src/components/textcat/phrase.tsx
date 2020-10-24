90427f3351ce16472680379fcd89ea4cb3598639
Simon Legner <Simon.Legner@gmail.com>
2020-10-24 08:02:40 +0200

    Select phrases

--- src/components/textcat/phrase.tsx
+++ src/components/textcat/phrase.tsx
@@ -1,5 +1,5 @@
 import { FunctionalComponent, h } from "preact";
-import { useContext } from "preact/hooks";
+import { useContext, useState } from "preact/hooks";
 import { Catalog } from "./catalog";
 import * as style from "./style.css";
 
@@ -10,24 +10,36 @@ interface Props {
 const TextcatPhrase: FunctionalComponent<Props> = (props: Props) => {
   const catalog = useContext(Catalog);
   const phrase = catalog.phrase(props.phrase);
+  const [selectedLine, setSelectedLine] = useState(
+    phrase?.lines?.length === 1 ? 0 : -1
+  );
   if (!phrase) return <section></section>;
   return (
     <section class={style.block}>
       <header>{phrase.header.de}</header>
-      {phrase.lines.map(line => (
-        <p key={line} class={style.block}>
-          {catalog
-            .splitPhraseLine(line.line.de ?? "")
-            .map((word, index) =>
-              word.startsWith("{") ? (
-                <TextcatPhrase
-                  key={index}
-                  phrase={word.substring(1, word.length - 1)}
-                ></TextcatPhrase>
-              ) : (
-                <span key={index}>{word}</span>
-              )
-            )}
+      {phrase.lines.map((line, lineIndex) => (
+        <p
+          key={line}
+          class={style.block}
+          onClick={(): void => setSelectedLine(lineIndex)}
+        >
+          {selectedLine !== lineIndex
+            ? line.line.de
+            : [
+                "🗹 ",
+                catalog
+                  .splitPhraseLine(line.line.de ?? "")
+                  .map((word, index) =>
+                    word.startsWith("{") ? (
+                      <TextcatPhrase
+                        key={index}
+                        phrase={word.substring(1, word.length - 1)}
+                      ></TextcatPhrase>
+                    ) : (
+                      <span key={index}>{word}</span>
+                    )
+                  )
+              ]}
         </p>
       ))}
     </section>
