dcda3950ba3775f867319f798b8d28d9a758df4d
Simon Legner <Simon.Legner@gmail.com>
2020-11-11 16:55:55 +0100

    AddSentencePane: add sentence prefix search

--- src/components/textcat/addSentencePane.tsx
+++ src/components/textcat/addSentencePane.tsx
@@ -1,8 +1,9 @@
 import { FunctionalComponent, h } from "preact";
-import { useContext, useState } from "preact/hooks";
+import { useContext, useMemo, useState } from "preact/hooks";
 import { defaultNewSentenceCurlyName } from "../../model";
 import { CatalogContext } from "./contexts";
 import plusSquare from "bootstrap-icons/icons/plus-square.svg";
+import search from "bootstrap-icons/icons/search.svg";
 
 interface Props {
   addSentence: (curlyName: string) => void;
@@ -11,27 +12,53 @@ interface Props {
 const AddSentencePane: FunctionalComponent<Props> = (props: Props) => {
   const [curlyName, setCurlyName] = useState(defaultNewSentenceCurlyName());
   const catalog = useContext(CatalogContext);
+  const [searchText, setSearchText] = useState("");
+  const filteredSentences = useMemo(
+    () =>
+      searchText ? catalog.searchSentences(searchText) : catalog.sentences,
+    [catalog, searchText]
+  );
 
   return (
-    <label>
-      {"[Sentence: "}
-      <select
-        value={curlyName}
-        onChange={(e): void =>
-          setCurlyName((e.target as HTMLSelectElement).value)
-        }
-      >
-        {catalog.sentences.map(({ curlyName, header }) => (
-          <option key={curlyName} value={curlyName}>
-            {header}
-          </option>
-        ))}
-      </select>{" "}
-      <button onClick={(): void => props.addSentence(curlyName)}>
-        <img src={plusSquare} width={16} height={16} />
-      </button>
-      {"]"}
-    </label>
+    <div>
+      <label>
+        {"[Search: "}
+        <input
+          type="text"
+          value={searchText}
+          onChange={(e): void =>
+            setSearchText((e.target as HTMLInputElement).value)
+          }
+        />
+        <button
+          onChange={(e): void =>
+            setSearchText((e.target as HTMLInputElement).value)
+          }
+        >
+          <img src={search} width={16} height={16} />
+        </button>
+        {"]"}
+      </label>{" "}
+      <label>
+        {"[Sentence: "}
+        <select
+          value={curlyName}
+          onChange={(e): void =>
+            setCurlyName((e.target as HTMLSelectElement).value)
+          }
+        >
+          {filteredSentences.map(({ curlyName, header }) => (
+            <option key={curlyName} value={curlyName}>
+              {header}
+            </option>
+          ))}
+        </select>{" "}
+        <button onClick={(): void => props.addSentence(curlyName)}>
+          <img src={plusSquare} width={16} height={16} />
+        </button>
+        {"]"}
+      </label>
+    </div>
   );
 };
 
