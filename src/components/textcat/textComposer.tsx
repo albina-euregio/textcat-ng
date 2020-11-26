8d7bf4d6f3346425ccf2d97d2e5a2ff0c55aee5b
Simon Legner <Simon.Legner@gmail.com>
2020-11-26 18:45:04 +0100

    TextComposer: disable up/down for first/last sentence

--- src/components/textcat/textComposer.tsx
+++ src/components/textcat/textComposer.tsx
@@ -35,28 +35,26 @@ const TextComposer: FunctionalComponent<Props> = (props: Props) => {
           }
         >
           {" "}
-          {index > 0 && (
-            <button
-              onClick={(): void => props.moveSentence(index, -1)}
-              title={t("sentence.moveUp")}
-            >
-              <img src={caretUpSquare} width={16} height={16} />
-            </button>
-          )}
+          <button
+            disabled={index === 0}
+            onClick={(): void => props.moveSentence(index, -1)}
+            title={t("sentence.moveUp")}
+          >
+            <img src={caretUpSquare} width={16} height={16} />
+          </button>
           <button
             onClick={(): void => props.moveSentence(index, 0)}
             title={t("sentence.remove")}
           >
             <img src={xSquare} width={16} height={16} />
           </button>
-          {index < array.length - 1 && (
-            <button
-              onClick={(): void => props.moveSentence(index, +1)}
-              title={t("sentence.moveDown")}
-            >
-              <img src={caretDownSquare} width={16} height={16} />
-            </button>
-          )}
+          <button
+            disabled={index >= array.length - 1}
+            onClick={(): void => props.moveSentence(index, +1)}
+            title={t("sentence.moveDown")}
+          >
+            <img src={caretDownSquare} width={16} height={16} />
+          </button>
         </PhraseComposer>
       ))}
     </section>
