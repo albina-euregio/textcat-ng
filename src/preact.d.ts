61efa3f32702b21eebdae12d7a0caeaa67771b71
Simon Legner <Simon.Legner@gmail.com>
2024-01-19 22:06:15 +0100

    refact: update tsconfig per vitejs.dev/guide

--- src/preact.d.ts
+++ src/preact.d.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import JSX = preact.JSX;
+/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   VITE_DEEPL_API?: string;
