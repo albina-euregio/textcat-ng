5a90f4972f3f83ffaa76a352325934421e505694
Simon Legner <Simon.Legner@gmail.com>
2024-11-18 18:53:09 +0100

    refact: useTraceUpdate for debugging

--- src/components/textcat/useTraceUpdate.tsx
+++ src/components/textcat/useTraceUpdate.tsx
@@ -1,13 +1,13 @@
 import { useRef, useEffect } from "preact/hooks";
 
-export function useTraceUpdate(props: Record<string, unknown>) {
+export function useTraceUpdate(label: string, props: Record<string, unknown>) {
   const prev = useRef(props);
   useEffect(() => {
     const changedProps = Object.entries(props)
       .filter(([k, v]) => prev.current[k] !== v)
-      .map(([k, v]) => [k, prev.current[k], v]);
+      .flatMap(([k, v]) => [k, prev.current[k], v]);
     if (changedProps.length > 0) {
-      console.log("Changed props:", changedProps);
+      console.log("Changed props:", label, ...changedProps);
     }
     prev.current = props;
   });
