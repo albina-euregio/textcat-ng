183eaaba3f5496a73561fb4cf21438886fb995a5
Simon Legner <Simon.Legner@gmail.com>
2024-11-27 22:32:11 +0100

    i18n: import.meta.glob

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,25 +1,9 @@
 import { srcLang } from "../components/state";
-import { type Lang } from "../model";
 
-import ca from "./ca.json";
-import de from "./de.json";
 import en from "./en.json";
-import es from "./es.json";
-import fr from "./fr.json";
-import it from "./it.json";
-import oc from "./oc.json";
+const i18nMessages = import.meta.glob("./*.json", { eager: true });
 
 type I18nKeys = keyof typeof en;
-
-const i18nMessages: Partial<Record<Lang, Record<I18nKeys, string>>> = {
-  ca,
-  de,
-  en,
-  es,
-  fr,
-  it,
-  oc
-};
 
 /**
  * Returns the translation for the given message key.
@@ -28,7 +12,7 @@ const i18nMessages: Partial<Record<Lang, Record<I18nKeys, string>>> = {
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function t(key: I18nKeys, ...args: any[]): string {
-  const messages = i18nMessages[srcLang.value] || i18nMessages.en;
-  const message = messages?.[key] || i18nMessages.en?.[key] || key;
+  const messages = i18nMessages[`./${srcLang.value}.json`]?.default;
+  const message = messages?.[key] || en?.[key] || key;
   return message.replace(/\$(\d+)/g, (_, index) => args[index - 1]);
 }
