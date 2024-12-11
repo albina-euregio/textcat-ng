37c7b05c7e9d5effd1b2d633f0793f49c6dca4b5
Simon Legner <Simon.Legner@gmail.com>
2024-12-11 15:15:09 +0100

    i18n: import.meta.glob (import=default)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,13 @@
 import { srcLang } from "../components/state";
+import { Lang } from "../model";
 
 import en from "./en.json";
-const i18nMessages = import.meta.glob("./*.json", { eager: true });
-
 type I18nKeys = keyof typeof en;
+
+const i18nMessages = import.meta.glob("./*.json", {
+  eager: true,
+  import: "default"
+}) as Record<`./${Lang}.json`, I18nKeys>;
 
 /**
  * Returns the translation for the given message key.
@@ -12,7 +16,7 @@ type I18nKeys = keyof typeof en;
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function t(key: I18nKeys, ...args: any[]): string {
-  const messages = i18nMessages[`./${srcLang.value}.json`]?.default;
+  const messages = i18nMessages[`./${srcLang.value}.json`];
   const message = messages?.[key] || en?.[key] || key;
   return message.replace(/\$(\d+)/g, (_, index) => args[index - 1]);
 }
