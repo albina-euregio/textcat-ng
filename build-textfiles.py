bb1bd6f402310b31b02776802ca8a3ee96b45783
Simon Legner <Simon.Legner@gmail.com>
2025-01-27 12:40:43 +0100

    build-textfiles: ruff

--- build-textfiles.py
+++ build-textfiles.py
@@ -1,25 +1,24 @@
 #!/bin/env python3
-import codecs
 import pathlib
 import typing
 
 
 def textfiles(lang: str):
-    return pathlib.Path('../satzkatalog/data/').glob(f'{lang}/**/*.txt')
+    return pathlib.Path("../satzkatalog/data/").glob(f"{lang}/**/*.txt")
 
 
 def concat_textfile(file: pathlib.Path, dst: typing.IO):
-    with file.open(encoding='utf-8-sig') as src:
+    with file.open(encoding="utf-8-sig") as src:
         for line in src.readlines():
-            line = line.strip('\r\n')
-            line = line.replace('Line: Begin: ', 'Begin: ')
-            line = line.replace('Line: End: ', 'End: ')
+            line = line.strip("\r\n")
+            line = line.replace("Line: Begin: ", "Begin: ")
+            line = line.replace("Line: End: ", "End: ")
             print(line, file=dst)
 
 
-for lang in 'CA DE EN ES FR IT OC'.split():
-    dst_file = pathlib.Path(f'./public/assets/satzkatalog.{lang}.txt')
-    with dst_file.open(mode='w', encoding='utf-8') as dst:
-        print('Building', dst)
+for lang in "CA DE EN ES FR IT OC".split():
+    dst_file = pathlib.Path(f"./public/assets/satzkatalog.{lang}.txt")
+    with dst_file.open(mode="w", encoding="utf-8") as dst:
+        print("Building", dst)
         for file in sorted(textfiles(lang)):
             concat_textfile(file, dst)
