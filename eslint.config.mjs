c6b4b01ef840ab1d8b8462ec2b419af61c26e665
Simon Legner <Simon.Legner@gmail.com>
2025-08-20 08:22:47 +0200

    Update eslint to 8.33.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,15 @@
-import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import { includeIgnoreFile } from "@eslint/compat";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs
+} from "@vue/eslint-config-typescript";
 import pluginVue from "eslint-plugin-vue";
+import { fileURLToPath } from "node:url";
 
-export default [...pluginVue.configs["flat/essential"], ...vueTsEslintConfig()];
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfigWithVueTs([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended
+]);
