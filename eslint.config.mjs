af6bcba23280548e55dd941a7fb38587066ab15e
Simon Legner <Simon.Legner@gmail.com>
2026-04-04 17:35:45 +0200

    Upgrade eslint to 10.0.1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-import { includeIgnoreFile } from "@eslint/compat";
 import {
   defineConfigWithVueTs,
   vueTsConfigs,
 } from "@vue/eslint-config-typescript";
 import prettier from "eslint-config-prettier/flat";
 import pluginVue from "eslint-plugin-vue";
-import { fileURLToPath } from "node:url";
-
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfigWithVueTs([
-  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  { ignores: ["dist"] },
   pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
   prettier,
